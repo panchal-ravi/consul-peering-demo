b2430ec0f933d86a227ac596804c87d4dd155be2
Ravi Panchal <ravi.panchal@hashicorp.com>
2024-05-21 23:04:08 +0800

    Removed local-failover.sh

--- local-failover.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f examples/applications/pcss-demo/search/service-intentions-product.yml --context $S2
-kubectl delete -f examples/applications/pcss-demo/search/product1a-resolver.yml --context $S2
-kubectl delete -f examples/applications/pcss-demo/search/product-failover.yml --context $S2
-kubectl delete -f examples/applications/pcss-demo/search/product-splitter.yml --context $S2
-
